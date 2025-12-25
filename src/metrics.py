4bda7dcc2213ce011c2afe1bfcc1dbdbf7c49d2f
Marcus Backstrom <VBMarcusVB@hotmail.com>
2025-12-25 13:52:24 +0100

    moved to viz.pyt

--- src/metrics.py
+++ src/metrics.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 # Innehåller funktioner för databeräkningar, aggregeringar och transformationer (ingen visualisering).
 
 def product_per_category(df: pd.DataFrame) -> pd.DataFrame:
@@ -13,3 +13,12 @@ def year_month_combined(df: pd.DataFrame) -> pd.DataFrame:
     df.drop(columns=["OrderYear", "OrderMonth"], inplace=True)
     df = df[["YearMonth", "Revenue"]]
     return df
+
+def number_over_bar(df, target1, target2) -> plt:
+    fig ,ax = plt.subplots(figsize=(12,4))
+    bars = df[f"[{target1}]",f"[{target2}]"]
+    for bar in bars:
+        height = bar.get_height()
+        ax.text(bar.get_x() + bar.get_width()/2, height +3,
+                str(int(height)), ha="center", va="bottom", color="Black", fontweight="bold")
+    return
